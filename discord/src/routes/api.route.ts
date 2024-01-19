16b37d024b5cd8794f177744c853e6ed99094a7b
SkoSik <skos2002d@gmail.com>
2024-01-20 05:43:51 +0800

    test

--- discord/src/routes/api.route.ts
+++ discord/src/routes/api.route.ts
@@ -4,10 +4,13 @@ import discordClient from "@/discord";
 const ApiRouter: Router = Router();
 
 ApiRouter.post("/deleteRole", async (req, res) => {
+    console.log(req.body);
     let guild = discordClient.guilds.cache.get(req.body.server_id)
     let member = guild?.members.cache.get(req.body.user_id)
 
-    await member.roles.remove(req.body.role_id)
+    await member.roles.remove(req.body.role_id).then(() => {
+        res.status(200).send("")
+    })
 })
 
 export default ApiRouter;
