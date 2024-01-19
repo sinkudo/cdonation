88e28eac6fe0ac86058556a80dec56711b38e746
Yar <mupy.y.mup@yandex.ru>
2024-01-19 20:07:37 +0800

    price getter

--- test/Payments.test.js
+++ test/Payments.test.js
@@ -1,48 +1,54 @@
-const { expect } = require("chai")
-const { ethers } = require("hardhat")
+// Payments.test.js
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("Payments", function () {
-  let acc1
-  let acc2
-  let payments
-  let paymentsaddress
+describe("Payments Contract", function () {
+  let paymentsContract;
+  let owner;
+  let user1;
+  let user2;
 
-  beforeEach(async function() {
-    [acc1, acc2] = await ethers.getSigners()
-    console.log("сигнеры")
-    console.log(acc1, acc2)
-    const Payments = await ethers.getContractFactory("Payments", acc1)
-    payments = await Payments.deploy()
-    await payments.waitForDeployment()
-    // paymentsaddress = await payments.getAddress()
-    // console.log("пэймент", payments)
-    // console.log("address", await payments.getAddress())
-    // await payments.deployed()
-  })
+  beforeEach(async function () {
+    [owner, user1, user2] = await ethers.getSigners();
+  
+    const Payments = await ethers.getContractFactory("Payments");
+    paymentsContract = await Payments.deploy();
+    await paymentsContract.deployTransaction.wait(); // Wait for the deployment transaction to be mined
+  });
 
-  it("should be deployed", async function() {
-    console.log("depl", await payments.getAddress())
-    expect(await payments.getAddress()).to.be.properAddress
-  })
+  it("should deposit funds", async function () {
+    const value = ethers.utils.parseEther("2.0");
 
-  it("should have 0 ether by default", async function() {
-    const balance = await payments.currentBalance()
-    expect(balance).to.eq(0)
-  })
+    // Deposit funds
+    await paymentsContract.connect(user1).deposit({ value });
 
-  it("should be possible to send funds", async function() {
-    const sum = 100
-    const msg = "hello from hardhat"
-    const tx = await payments.connect(acc2).pay(msg, { value: sum })
+    // Get all payments
+    const allPayments = await paymentsContract.getAllPayments();
 
-    await expect(() => tx)
-      .to.changeEtherBalances([acc2, payments], [-sum, sum]);
+    // Assert the deposit is logged
+    expect(allPayments.length).to.equal(1);
+    expect(allPayments[0].from).to.equal(user1.address);
+    expect(allPayments[0].to).to.equal(paymentsContract.address);
+    expect(allPayments[0].value).to.equal(value);
+  });
 
-    await tx.wait()
 
-    const newPayment = await payments.getPayment(acc2.address, 0)
-    expect(newPayment.message).to.eq(msg)
-    expect(newPayment.amount).to.eq(sum)
-    expect(newPayment.from).to.eq(acc2.address)
-  })
-})
+  it("should withdraw all funds", async function () {
+    const initialBalance = await ethers.provider.getBalance(owner.address);
+    const depositValue = ethers.utils.parseEther("2.0");
+
+    // Deposit funds
+    await paymentsContract.connect(user1).deposit({ value: depositValue });
+
+    // Withdraw all funds
+    await paymentsContract.connect(owner).withdrawAll();
+
+    // Assert the withdrawal is logged
+    const allPayments = await paymentsContract.getAllPayments();
+    expect(allPayments.length).to.equal(2); // One for deposit and one for withdrawal
+
+    // Assert the balance of the owner after withdrawal
+    const finalBalance = await ethers.provider.getBalance(owner.address);
+    expect(finalBalance.sub(initialBalance)).to.equal(depositValue);
+  });
+});
